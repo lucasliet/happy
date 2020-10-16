49ade8aaa4c324587101933a9973f2d0d99618d9
lucasliet <lucasouliveira@gmail.com>
2020-10-16 13:23:48 -0300

    add listen to all front end urls

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import './database/connection';
 
@@ -7,6 +8,7 @@ import routes from './routes';
 import errorHandler from './errors/handler';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
