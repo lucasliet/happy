91126a4dc6bf1e82e8776e71cfcab2f109a2d583
lucasliet <lucasouliveira@gmail.com>
2020-10-16 12:01:31 -0300

    create orphanage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,45 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 import './database/connection';
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (request,response) => (
-  response.json({ message: '🚀 Server is Running' })
-));
+const port = 3333;
 
-app.listen(3333);
+app.get('/', (request, response) => {
+  console.log(`🚀 Server is Running on port ${port}`);
+  return response.json({ message: '🚀 Server is Running' })
+});
+
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.status(201).json(orphanage);
+})
+
+app.listen(port);
