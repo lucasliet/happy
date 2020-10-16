2c9d7b30061fb2636fa9ed0e123a6f78434ac417
lucasliet <lucasouliveira@gmail.com>
2020-10-16 13:21:55 -0300

    express async errors capture and validation

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
+import * as Yup from 'yup';
+
 const orphanagesRepository = getRepository(Orphanage);
 
 export default {
@@ -42,7 +44,7 @@ export default {
       {path: image.filename}
     ));
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -51,7 +53,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+          path: Yup.string().required()
+        })
+      ),
     });
+
+    await schema.validate(data, {
+      //show all fields errors
+      abortEarly: false,
+    });
+
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import 'express-async-errors';
 
 import './database/connection';
 
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 const port = 3333;
 
