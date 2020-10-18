91c72f021d8538deeec1f9c3de5d457519a856c8
lucasliet <lucasouliveira@gmail.com>
2020-10-18 19:25:03 -0300

    imagur try

---
+++ backend/src/config/imgurApi.ts
@@ -0,0 +1,4 @@
+import { Client } from '@rmp135/imgur'
+
+const api = new Client({ client_id: '3fe85ea6714be32' });
+export default api;
--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
+import FileStream from 'fs';
 
 import * as Yup from 'yup';
+
+import imgur from '../config/imgurApi';
 
 const orphanagesRepository = getRepository(Orphanage);
 
@@ -19,7 +22,7 @@ export default {
   async show(request: Request, response: Response) {
     const { id } = request.params;
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id ,{
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
       relations: ['images']
     });
 
@@ -36,13 +39,24 @@ export default {
       opening_hours,
       open_on_weekends
     } = request.body;
-  
-    //that as is a hack to upload multiple files
-    const requestImages = request.files as Express.Multer.File[]; 
 
-    const images = requestImages.map(image => (
-      {path: image.filename}
-    ));
+    //that as is a hack to upload multiple files
+    const requestImages = request.files as Express.Multer.File[];
+
+    const images: Array<{ path: string }> = await Promise.all(requestImages.map(async (image) => {
+      const file_base64 = FileStream.readFileSync(image.path)
+        .toString('base64');
+
+      const { data: { link: uploadedImageUrl } } = await imgur.Image.upload(
+        file_base64,
+        {
+          type: 'base64',
+          title: image.filename,
+        }
+      );
+      
+      return { path: uploadedImageUrl };
+    }));
 
     const data = {
       name,
@@ -64,8 +78,8 @@ export default {
       opening_hours: Yup.string().required(),
       open_on_weekends: Yup.boolean().required(),
       images: Yup.array(Yup.object().shape({
-          path: Yup.string().required()
-        })
+        path: Yup.string().required()
+      })
       ),
     });
 
@@ -75,7 +89,7 @@ export default {
     });
 
     const orphanage = orphanagesRepository.create(data);
-  
+
     await orphanagesRepository.save(orphanage);
 
     return response.status(201).json(orphanage);
